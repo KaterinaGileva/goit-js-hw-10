9ad43ebb0a51f4b35e1eff68c6bd93231b0bf9bf
Екатерина <Katderl@ukr.net>
2022-09-24 15:27:15 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,14 @@
-  export { fetchCountries };
+ // export { fetchCountries };
 
-function fetchCountries(countryName) {
-    return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-     .then(response => {
-       if (response.status !== 200) {
-           throw new Error(response.status);;
-       } 
-       return response.json();
-     });
+//function fetchCountries(countryName) {
+   // return fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    // .then(response => {
+    //   if (response.status !== 200) {
+   //        throw new Error(response.status);;
+   //    } 
+   //    return response.json();
+   //  });
      
-   }
+ //  }
 
    
